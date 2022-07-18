24fd056e4901d4eef2278cdc0b48d04a63da4eff
Leo <kovzel.leonid@mail.ru>
2022-07-18 17:02:40 +0300

    Add redirect if logged

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import UsersContainer from "./components/Users/UserContainer";
-import {LoginForm} from "./Login/LoginForm";
+import LoginForm from "./Login/LoginForm";
 
 
 const App: React.FC = () => {
--- src/Login/LoginForm.tsx
+++ src/Login/LoginForm.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import {SubmitHandler, useForm} from "react-hook-form";
 import "./Login.module.css"
 import f from "./Login.module.css"
-import {login} from "../redux/authReducer";
-import {useDispatch} from "react-redux";
+import {login, loginDataType} from "../redux/authReducer";
+import {connect} from "react-redux";
+import {AppStateType} from "../redux/reduxStore";
+import {Navigate} from "react-router-dom";
 
-export const LoginForm = () => {
+type LoginFormType = {
+	login: (loginData: loginDataType) => void,
+	isAuth: boolean
+}
+
+type mapStateType = {
+	isAuth: boolean
+}
+
+const LoginForm = (props: LoginFormType) => {
 
 	type Inputs = {
 		email: string,
@@ -13,7 +24,6 @@ export const LoginForm = () => {
 		rememberMe: boolean,
 	};
 
-	const dispatch = useDispatch()
 
 	const {
 		register, // Регистрация полей для формы
@@ -23,60 +33,70 @@ export const LoginForm = () => {
 	} = useForm<Inputs>({mode: "onBlur"});
 	const onSubmit: SubmitHandler<Inputs> = (loginData) => {
 		console.log(loginData)
-		dispatch(login(loginData))
+		props.login(loginData)
 		reset()
 	};
 
 	return <>
-		<h1 className={f.login}>Login</h1>
-		<form onSubmit={handleSubmit(onSubmit)} className={f.form}>
-			{/*LOGIN INPUT*/}
-			<div>
-				<label className={f.labelTextInput}>
-					Login
-					<input className={` ${errors.email ? f.errorBorder : f.textInput} `} {...register("email",
-						{
-							required: "The field is required",
-							minLength: {
-								value: 5,
-								message: "Minimum 5 characters"
+		{props.isAuth ? <Navigate to='/profile'/> :
+			<>
+				<h1 className={f.login}>Login</h1>
+				<form onSubmit={handleSubmit(onSubmit)} className={f.form}>
+					{/*LOGIN INPUT*/}
+					<div>
+						<label className={f.labelTextInput}>
+							Login
+							<input className={` ${errors.email ? f.errorBorder : f.textInput} `} {...register("email",
+								{
+									required: "The field is required",
+									minLength: {
+										value: 5,
+										message: "Minimum 5 characters"
 
-							},
-						})} type="text"/>
-				</label>
-				<div>
-					{errors?.email && <p className={f.error}>{errors?.email?.message || "Error!"}</p>}
-				</div>
-			</div>
-			{/*PASSWORD INPUT*/}
-			<div>
-				<label className={f.labelTextInput}>
-					Password
-					<input className={` ${errors.password ? f.errorBorder : f.textInput} `} {...register("password",
-						{
-							required: "The field is required",
-							minLength: {
-								value: 5,
-								message: "Minimum 5 characters"
+									},
+								})} type="text"/>
+						</label>
+						<div>
+							{errors?.email && <p className={f.error}>{errors?.email?.message || "Error!"}</p>}
+						</div>
+					</div>
+					{/*PASSWORD INPUT*/}
+					<div>
+						<label className={f.labelTextInput}>
+							Password
+							<input className={` ${errors.password ? f.errorBorder : f.textInput} `} {...register("password",
+								{
+									required: "The field is required",
+									minLength: {
+										value: 5,
+										message: "Minimum 5 characters"
 
-							},
-						})} type="text"/>
-				</label>
-				<div>
-					{errors?.password && <p className={f.error}>{errors?.password?.message || "Error!"}</p>}
-				</div>
-			</div>
-			{/*CHECKBOX INPUT*/}
-			<div>
-				<label className={f.labelCheckbox}>
-					Remember me
-					<input {...register("rememberMe")} type="checkbox"/>
-				</label>
-			</div>
-			{/*SUBMIT INPUT*/}
-			<div>
-				<input className={f.submitInput} type="submit" value="SUBMIT" disabled={!isValid}/>
-			</div>
-		</form>
+									},
+								})} type="text"/>
+						</label>
+						<div>
+							{errors?.password && <p className={f.error}>{errors?.password?.message || "Error!"}</p>}
+						</div>
+					</div>
+					{/*CHECKBOX INPUT*/}
+					<div>
+						<label className={f.labelCheckbox}>
+							Remember me
+							<input {...register("rememberMe")} type="checkbox"/>
+						</label>
+					</div>
+					{/*SUBMIT INPUT*/}
+					<div>
+						<input className={f.submitInput} type="submit" value="SUBMIT" disabled={!isValid}/>
+					</div>
+				</form>
+			</>
+		}
 	</>
 }
+
+const mapStateToProps = (state: AppStateType): mapStateType => ({
+	isAuth: state.auth['isAuth']
+})
+
+export default connect(mapStateToProps, {login})(LoginForm)

a3756368a9eb591e1be8af7f28e0973076d9400a
Leonid Kovzel <kovzel.leo@gmail.com>
2022-09-13 18:07:03 +0300

    feat: add viewports and media query for navbar

--- src/common/components/navbar/ItemMenu.tsx
+++ src/common/components/navbar/ItemMenu.tsx
@@ -23,7 +23,7 @@ export const ItemMenu: FC<PropsType> = memo(
         to={link}
       >
         <img src={img} height="50px" width="50px" alt={'itemMenu'} />
-        {title}
+        <div className={s.menuTitle}> {title}</div>
         {activeItemMenu === name ? (
           <div className={s.boxVerticalLine_active}>
             <div className={s.verticalLine_active} />
